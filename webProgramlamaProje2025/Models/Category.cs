7fc49499aa30f705d8f83298a125ddf758322ebd
cetinumit <umitc5534@gmail.com>
2025-12-12 23:49:46 +0300

    Veritabanı altyapısı için Category modeli eklendi

---
+++ webProgramlamaProje2025/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace webProgramlamaProje2025.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
